ce6b0a542dfc938d1cec4413a1f23b5cb1e32ab2
IKostric <ivica_kostric@yahoo.com>
2025-10-29 12:43:34 +0100

    chore: stop tracking /crs, /data, /tests; update .gitignore

--- crs/retrieval/__init__.py
+++
@@ -1,5 +0,0 @@
-# CRS Retrieval module
-from .retrieval import ItemRetriever
-from .retrieval_utils import get_retriever, safe_retrieve
-
-__all__ = ["ItemRetriever", "get_retriever", "safe_retrieve"]
